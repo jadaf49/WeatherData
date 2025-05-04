4adf88a507162f59a7a66da90b9e4ea4610d804a
jadaf49 <jadaf49@nycstudents.net>
2025-05-04 16:31:48 +0000

    successful longestheatWave method & test cases

--- Main.java
+++ Main.java
@@ -4,16 +4,27 @@ public class Main
 {
     public static void main(String[]args)
     {
-        double []temps= {99.1,142.0,85.0,85.1,84.6,94.3,124.9,98.0,101.0,102.5};
-        ArrayList<Double> t=new ArrayList<Double>();
-        for(double n:temps)t.add(n);
-        WeatherData w=new WeatherData(t);
-        w.cleanData(85.0,120.0);
-        System.out.println(w);
-        t.add(1,121.0);
-        t.add(1,84.0);
-        System.out.println(w);
-        w.cleanData(85.0,120.0);
-        System.out.println(w);
+        //double []temps= {99.1,142.0,85.0,85.1,84.6,94.3,124.9,98.0,101.0,102.5};
+        //ArrayList<Double> t=new ArrayList<Double>();
+        //for(double n:temps)t.add(n);
+        //WeatherData w=new WeatherData(t);
+        //w.cleanData(85.0,120.0);
+        //System.out.println(w);
+        //t.add(1,121.0);
+        //t.add(1,84.0);
+        //System.out.println(w);
+        //w.cleanData(85.0,120.0);
+        //System.out.println(w);
+
+        double []newTemps= {100.5,98.5,102,103.9,87.5,105.2,90.3,94.8,109.1,102.1,107.4,93.2};
+        ArrayList<Double> newList=new ArrayList<Double>();
+        for(double n:newTemps)newList.add(n);
+        WeatherData w = new WeatherData(newList);
+        System.out.println(w.longestHeatWave(100.5));
+        System.out.println(w.longestHeatWave(95.2));
+        
+
+
+
     }
 }
--- WeatherData.java
+++ WeatherData.java
@@ -27,12 +27,31 @@ public class WeatherData
     * Precondition: There is at least one heat wave in temperatures based on threshold.
     */
     public int longestHeatWave(double threshold)
-    { /* to be implemented in part (b) */ }
+    { 
+        int count=0;
+        int max=0;
+        double current=0;
+        for (int i=0; i<temperatures.size();i++)
+        {
+            current = temperatures.get(i);
+            if (current>threshold)
+            {
+                count++;
+            }
+            if (current<=threshold)
+            {
+                if (count>max) max=count;
+                count=0;
+            }
+
+        }
+        return max;
+    }
     // There may be instance variables, constructors, and methods that are not shown.
 
     public String toString()
     {
         return temperatures.toString();
-        
+
     }
 }
